c1ef8a64883261f4badcb6dd0f2bc6df84df91e6
3damp <amp206@gmail.com>
2024-12-01 11:07:19 +0000

    update game

--- Build/pwa.framework.js
+++ Build/pwa.framework.js
@@ -1281,10 +1281,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  7558576: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 7558631: ($0) => { performance.now = function() { return $0; }; },  
- 7558679: ($0) => { performance.now = function() { return $0; }; },  
- 7558727: () => { performance.now = Module['emscripten_get_now_backup']; }
+  7558608: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 7558663: ($0) => { performance.now = function() { return $0; }; },  
+ 7558711: ($0) => { performance.now = function() { return $0; }; },  
+ 7558759: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "PetTest-0.0.3";
+const CACHE_NAME = "PetTest-0.0.4";
 const contentToCache = [
   "Build/pwa.loader.js",
   "Build/pwa.framework.js",
